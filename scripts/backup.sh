7f1b41d018bd569f309ba284443120a4dfcc14f8
Mano Sriram <mano.sriram0@gmail.com>
2025-10-10 02:48:52 +0530

    chore: Do not write backup.sh to a log file, use cronicle instead

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,13 +6,6 @@ source ~/.bashrc;
 [ -z "$1" ] && { echo "Usage: $0 <tag>"; exit 1; }
 TAG="$1" # Assign the provided tag
 
-# Define log directory and create it if it doesn't exist
-LOG_DIR="/var/log/restic_backups"
-mkdir -p "$LOG_DIR"
-
-# Create a timestamped log file
-LOG_FILE="$LOG_DIR/$(date +%d-%m-%Y-%H%M%S).log"
-
 # Paths to backup
 BACKUP_PATHS="/fs/containers/immich/ /fs/containers/tubearchivist/ /fs/containers/vaultwarden/ /fs/containers/scripts/ /fs/backups"
 
@@ -20,25 +13,24 @@ DAILY_HEALTHCHECKS_URL="https://hc-ping.com/5d70a7da-b8ca-4571-a059-839cff1fb6d0
 MONTHLY_HEALTHCHECKS_URL="https://hc-ping.com/f03e366d-22a9-414a-8039-32d17b1dc632"
 
 # Perform backup and log status
-restic backup --tag "$TAG" $BACKUP_PATHS &>> "$LOG_FILE" \
-	&& echo "Backup with tag '$TAG' completed." | tee -a "$LOG_FILE" \
-	|| { echo "Backup with tag '$TAG' FAILED! Check $LOG_FILE." | tee -a "$LOG_FILE"; exit 1; }
+restic backup --tag "$TAG" $BACKUP_PATHS \
+	&& echo "Backup with tag '$TAG' completed."
 
 # Conditional curl notification based on tag
 if [[ "$TAG" == "daily" ]]; then
-	curl -s -X POST -H 'Content-Type: application/json' -d '{"text":"Daily backup completed!"}' $DAILY_HEALTHCHECKS_URL &>> "$LOG_FILE" \
-		&& echo "Daily curl notification sent." | tee -a "$LOG_FILE"
+	curl -s -X POST -H 'Content-Type: application/json' -d '{"text":"Daily backup completed!"}' $DAILY_HEALTHCHECKS_URL \
+		&& echo "Daily curl notification sent."
 elif [[ "$TAG" == "monthly" ]]; then
-	curl -s -X POST -H 'Content-Type: application/json' -d '{"text":"Monthly backup completed!"}' $MONTHLY_HEALTHCHECKS_URL &>> "$LOG_FILE" \
-		&& echo "Monthly curl notification sent." | tee -a "$LOG_FILE"
+	curl -s -X POST -H 'Content-Type: application/json' -d '{"text":"Monthly backup completed!"}' $MONTHLY_HEALTHCHECKS_URL \
+		&& echo "Monthly curl notification sent."
 fi
 
 # Apply retention for daily backups
-restic forget --tag daily --prune --keep-last 30 &>> "$LOG_FILE" \
-	&& echo "Daily retention applied." | tee -a "$LOG_FILE" \
-	|| echo "Daily retention FAILED! Check $LOG_FILE." | tee -a "$LOG_FILE"
+restic forget --tag daily --prune --keep-last 30 \
+	&& echo "Daily retention applied." \
+	|| echo "Daily retention FAILED!"
 
 # Apply retention for monthly backups
-restic forget --tag monthly --prune --keep-last 12 &>> "$LOG_FILE" \
-	&& echo "Monthly retention applied." | tee -a "$LOG_FILE" \
-	|| echo "Monthly retention FAILED! Check $LOG_FILE." | tee -a "$LOG_FILE"
+restic forget --tag monthly --prune --keep-last 12 \
+	&& echo "Monthly retention applied." \
+	|| echo "Monthly retention FAILED!"
